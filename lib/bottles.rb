b48a15d65a7acf8c170803cd0e8e83814cf936f8
Saull Brandão <saull@outlook.com>
2022-07-26 14:59:05 -0300

    refactor code

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,27 +8,49 @@ class Bottles
   end
 
   def verse(number)
-    case number
-    when 0
-      'No more bottles of beer on the wall, '\
-      "no more bottles of beer.\n"\
-      'Go to the store and buy some more, '\
-      "99 bottles of beer on the wall.\n"
-    when 1
-      '1 bottle of beer on the wall, ' \
-      "1 bottle of beer.\n" \
-      'Take it down and pass it around, ' \
-      "no more bottles of beer on the wall.\n"
-    when 2
-      '2 bottles of beer on the wall, ' \
-      "2 bottles of beer.\n" \
-      'Take one down and pass it around, ' \
-      "1 bottle of beer on the wall.\n"
+    "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " \
+    "#{quantity(number)} #{container(number)} of beer.\n" \
+    "#{action(number)}, " \
+    "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
+  end
+
+  def container(number)
+    if number == 1
+      'bottle'
     else
-      "#{number} bottles of beer on the wall, " \
-      "#{number} bottles of beer.\n" \
-      'Take one down and pass it around, ' \
-      "#{number - 1} bottles of beer on the wall.\n"
+      'bottles'
+    end
+  end
+
+  def pronoum(number)
+    if number == 1
+      'it'
+    else
+      'one'
+    end
+  end
+
+  def quantity(number)
+    if number.zero?
+      'no more'
+    else
+      number.to_s
+    end
+  end
+
+  def action(number)
+    if number.zero?
+      'Go to the store and buy some more'
+    else
+      "Take #{pronoum(number)} down and pass it around"
+    end
+  end
+
+  def successor(number)
+    if number.zero?
+      99
+    else
+      number - 1
     end
   end
 end
