f301df1c1eecf7abb2d9cebfbf31d7ddb2b3cfaf
Saull Brandão <saull@outlook.com>
2022-07-27 11:04:10 -0300

    implements the six-pack feature

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -30,6 +30,8 @@ class BottleNumber
       BottleNumber0
     when 1
       BottleNumber1
+    when 6
+      BottleNumber6
     else
       BottleNumber
     end.new(number)
@@ -81,5 +83,15 @@ class BottleNumber1 < BottleNumber
 
   def pronoum
     'it'
+  end
+end
+
+class BottleNumber6 < BottleNumber
+  def container
+    'six-pack'
+  end
+
+  def quantity
+    '1'
   end
 end
