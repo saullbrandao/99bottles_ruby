5097171b2d0ad5ebfb79296552a295588c8632bf
Saull Brandão <saull@outlook.com>
2022-07-28 10:57:30 -0300

    auto register the factory's candidates

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -18,23 +18,30 @@ class Bottles
 end
 
 class BottleNumber
+  def self.for(number)
+    registry.find { |candidate| candidate.handles?(number) }.new(number)
+  end
+
+  def self.registry
+    @registry ||= [BottleNumber]
+  end
+
+  def self.register(candidate)
+    registry.prepend(candidate)
+  end
+
+  def self.inherited(candidate)
+    register(candidate)
+  end
+
+  def self.handles?(_number)
+    true
+  end
+
   attr_reader :number
 
   def initialize(number)
     @number = number
-  end
-
-  def self.for(number)
-    case number
-    when 0
-      BottleNumber0
-    when 1
-      BottleNumber1
-    when 6
-      BottleNumber6
-    else
-      BottleNumber
-    end.new(number)
   end
 
   def to_s
@@ -63,6 +70,10 @@ class BottleNumber
 end
 
 class BottleNumber0 < BottleNumber
+  def self.handles?(number)
+    number.zero?
+  end
+
   def quantity
     'no more'
   end
@@ -77,6 +88,10 @@ class BottleNumber0 < BottleNumber
 end
 
 class BottleNumber1 < BottleNumber
+  def self.handles?(number)
+    number == 1
+  end
+
   def container
     'bottle'
   end
@@ -87,6 +102,10 @@ class BottleNumber1 < BottleNumber
 end
 
 class BottleNumber6 < BottleNumber
+  def self.handles?(number)
+    number == 6
+  end
+
   def container
     'six-pack'
   end
