385286253b83db3876788a38c6d4f40a0e8be238
Saull Brandão <saull@outlook.com>
2022-07-29 11:14:51 -0300

    use dependency injection

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,4 +1,10 @@
 class Bottles
+  attr_reader :verse_template
+
+  def initialize(verse_template: BottleVerse)
+    @verse_template = verse_template
+  end
+
   def song
     verses(99, 0)
   end
@@ -8,20 +14,22 @@ class Bottles
   end
 
   def verse(number)
-    BottleVerse.new(number).lyrics
+    verse_template.lyrics(number)
   end
 end
 
 class BottleVerse
-  attr_reader :number
+  def self.lyrics(number)
+    new(BottleNumber.for(number)).lyrics
+  end
 
-  def initialize(number)
-    @number = number
+  attr_reader :bottle_number
+
+  def initialize(bottle_number)
+    @bottle_number = bottle_number
   end
 
   def lyrics
-    bottle_number = BottleNumber.for(number)
-
     "#{bottle_number} ".capitalize + 'of beer on the wall, ' \
     "#{bottle_number} of beer.\n" \
     "#{bottle_number.action}, " \
